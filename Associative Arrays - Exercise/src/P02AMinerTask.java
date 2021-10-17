b55e03565ce6993027c3cd6451e173109927b01c
Silviyaa <silviq.stefanova.sit6b@gmail.com>
2021-10-17 12:42:15 +0300

    Associative Arrays-Exercise

---
+++ Associative Arrays - Exercise/src/P02AMinerTask.java
@@ -0,0 +1,29 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class P02AMinerTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Integer> result = new LinkedHashMap<>();
+
+        String line = scanner.nextLine();
+
+        while (!line.equals("stop")) {
+            int quantity = Integer.parseInt(scanner.nextLine());
+
+            if (!result.containsKey(line)) {
+                result.put(line, quantity);
+            } else {
+                Integer replace = result.get(line) + quantity;
+                result.replace(line, replace);
+            }
+
+            line = scanner.nextLine();
+        }
+
+        for (Map.Entry<String, Integer> entry : result.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+}
