93091b62e922142c838fbe9a651b68aafd641268
Silviyaa <silviq.stefanova.sit6b@gmail.com>
2021-10-17 12:37:36 +0300

    Arrays

---
+++ Arrays/src/EvenAndOddSubtraction.java
@@ -0,0 +1,37 @@
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EvenAndOddSubtraction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split(" ");
+
+        // String[] -> int[]
+//        int[] numbers = new int[input.length];
+//        for (int i = 0; i < input.length; i++) {
+//            numbers[i] = Integer.parseInt(input[i]);
+//        }
+        int[] numbers = Arrays.stream(input)
+                .mapToInt(element -> Integer.parseInt(element))
+                .toArray();
+
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for(int number : numbers){
+            if(number % 2 ==0){
+                evenSum+=number;
+            } else {
+                oddSum+=number;
+            }
+        }
+
+        System.out.println(evenSum - oddSum);
+
+
+
+    }
+}
